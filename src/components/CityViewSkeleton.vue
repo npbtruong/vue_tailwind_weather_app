336dec77573495831ec1ee222fb04bb22106d7e8
npbtruong <142963392+npbtruong@users.noreply.github.com>
2024-01-09 09:00:14 +0700

    Update CityViewSkeleton.vue

--- src/components/CityViewSkeleton.vue
+++ src/components/CityViewSkeleton.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col flex-1">
       <!-- Overview -->
       <div class="flex flex-col py-12 items-center">
-        <AnimatePlaceHolder class="max-w-[300px] w-full mb-2" />
+       <!-- <AnimatePlaceHolder class="max-w-[300px] w-full mb-2" />
         <AnimatePlaceHolder class="max-w-[300px] w-full mb-12" />
         <AnimatePlaceHolder
           class="max-w-[300px] h-[100px] w-full mb-12"
@@ -10,23 +10,23 @@
         <AnimatePlaceHolder class="max-w-[300px] w-full mb-8" />
         <AnimatePlaceHolder
           class="max-w-[300px] h-[75px] w-full"
-        />
+        />-->
       </div>
       <!-- Hourly -->
       <div class="flex flex-col py-12 px-8 items-center">
-        <AnimatePlaceHolder
+        <!--<AnimatePlaceHolder
           class="max-w-screen-md h-[100px] w-full mb-12 "
-        />
+        /> -->
       </div>
       <!-- Weekly -->
       <div class="flex flex-col py-12 px-8 items-center">
-        <AnimatePlaceHolder
+        <!--<AnimatePlaceHolder
           class="max-w-screen-md h-[100px] w-full mb-12"
-        />
+        /> -->
       </div>
     </div>
   </template>
   
   <script setup>
-  import AnimatePlaceHolder from "./AnimatedPlaceHolder.vue";
+  //import AnimatePlaceHolder from "./AnimatedPlaceHolder.vue";
   </script>
